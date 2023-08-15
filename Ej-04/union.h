8caff8a7b9883a0ea316a1c5e608144e2a2eb9e4
esalcedo86 <140828775+esalcedo86@users.noreply.github.com>
2023-08-15 12:05:01 -0300

    Initial commit

---
+++ Ej-04/union.h
@@ -0,0 +1,10 @@
+#include "../Lista/Lista.h"
+
+#ifndef U02_LISTAS_EJ_04_UNION_H_
+#define U02_LISTAS_EJ_04_UNION_H_
+
+template <class T> Lista<T> *unir(const Lista<T> &lisA, const Lista<T> &lisB) {
+  return new Lista<T>();
+}
+
+#endif // U02_LISTAS_EJ_04_UNION_H_
---
+++ Ej-05/CircList.h
@@ -0,0 +1,6 @@
+#ifndef U02_LISTAS_EJ_05_CIRCLIST_H_
+#define U02_LISTAS_EJ_05_CIRCLIST_H_
+
+class CircList {};
+
+#endif // U02_LISTAS_EJ_05_CIRCLIST_H_
